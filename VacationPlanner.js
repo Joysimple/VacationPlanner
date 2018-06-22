696627ab83001e5286f7d3406de9a2491fbf2b59
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-22 13:13:47 -0700

    updating function clickEnter()

--- VacationPlanner.js
+++ VacationPlanner.js
@@ -9,12 +9,17 @@ enterButton.addEventListener("click", clickEnter);
 function clickEnter(){
     var daysInput = document.getElementById("days").value;
     if (daysInput.length > 0){
-        var hoursText = document.createElement("p");
+        var daysText = document.getElementById("daysMessage");
+        daysText.innerHTML = "That means you'll be traveling for " + daysInput + " days";
+        var hoursText = document.getElementById("hoursMessage");
         var hours = daysInput * 24;
-        hoursText.innerHTML = "That means you'll be traveling for " + hours + "hours";
-        var minutesText = document.createElement("p");
+        hoursText.innerHTML = "That means you'll be traveling for " + hours + " hours";
+        var minutesText = document.getElementById("minutesMessage");
         var minutes = hours * 60;
-        minutesText.innerHTML = "Which means you'll be traveling for " + minutes + "minutes!";
+        minutesText.innerHTML = "Which means you'll be traveling for " + minutes + " minutes!";
+        var secondsText = document.getElementById("secondsMessage");
+        var seconds = minutes * 60;
+        secondsText.innerHTML = "Which means you'll be traveling for " + seconds + " seconds!";
     }
     else {
         errorElement.innerHTML = "You have to input something";
