174127c697df35c0bc92bd2cef8e3e001fe58946
Svetlana Sokolova <Svetlana.Sokolova@gmail.com>
2018-06-26 09:27:09 -0700

    adding function clickNext

--- VacationPlanner.js
+++ VacationPlanner.js
@@ -1,22 +1,27 @@
-var nameInput = document.getElementById("greeting");
-var person = window.prompt("Please enter your name: ");
-var errorElement = document.getElementById("error");
-if (person != null) {
-    nameInput.innerHTML += ", " + person;
-} 
-var enterButton = document.getElementById("enterDays");
-enterButton.addEventListener("click", clickEnter);
-function clickEnter(){
+// Init section
+function init() {
+    var nameInput = document.getElementById("greeting");
+    var person = window.prompt("Please enter your name: ");
+    var errorElement = document.getElementById("error");
+    if (person != null) {
+        nameInput.innerHTML += ", " + person;
+    } 
+    var enterButton = document.getElementById("enterDays");
+    enterButton.addEventListener("click", clickEnter);
+    var nextButton = document.getElementById("timingNext");
+    nextButton.addEventListener("click", clickNext);    
+}
+
+// Handlers
+function clickEnter() {
     var daysInput = document.getElementById("days").value;
     if (daysInput.length > 0) {
         var daysText = document.getElementById("daysMessage");
         if (daysInput < 4) {
             daysText.innerHTML = "Short trips are always worth it!";
-        }
-        else if (daysInput < 7) {
+        } else if (daysInput < 7) {
             daysText.innerHTML = "Cool, you’ll be there for almost a week!";
-        }
-        else {
+        } else {
             daysText.innerHTML = "You’ll have plenty of time to relax and have fun!";
         }
         var hoursText = document.getElementById("hoursMessage");
@@ -28,9 +33,13 @@ function clickEnter(){
         var secondsText = document.getElementById("secondsMessage");
         var seconds = minutes * 60;
         secondsText.innerHTML = "Which means you'll be traveling for " + seconds + " seconds!";
-    }
-    else {
+    } else {
         errorElement.innerHTML = "You have to input something";
     }
+}
+function clickNext() {
     
 }
+
+// Main section
+init();
